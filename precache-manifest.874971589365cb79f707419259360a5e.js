154a47c5deb185c0b186f32cc1e7e21e15975bdd
JulianBrg <julian.barragan@alumno.uttehuacan.edu.mx>
2020-08-20 12:23:13 -0500

    Updates

---
+++ precache-manifest.874971589365cb79f707419259360a5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283803cd8664287c56d6dbf631fd575a",
+    "url": "/JournalApp/index.html"
+  },
+  {
+    "revision": "fe09496f7f51e38f5be7",
+    "url": "/JournalApp/static/css/main.184911c5.chunk.css"
+  },
+  {
+    "revision": "d696faf5ba7fc6b0c4b3",
+    "url": "/JournalApp/static/js/2.9cb5c413.chunk.js"
+  },
+  {
+    "revision": "981c68721fcc9cd3deecb8bc75096b7f",
+    "url": "/JournalApp/static/js/2.9cb5c413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe09496f7f51e38f5be7",
+    "url": "/JournalApp/static/js/main.2e0c46f4.chunk.js"
+  },
+  {
+    "revision": "9ffccb3c02a5d46c691e",
+    "url": "/JournalApp/static/js/runtime-main.cea6fe8d.js"
+  }
+]);
